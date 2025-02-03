ac5d346e0054412cde4e0df1287890dbb34f5842
The SunShining <thesunshiningmin@gmail.com>
2025-02-03 16:56:21 -0700

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,24 +4,26 @@ import { Link } from 'react-router-dom';
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.socialLink}>
-        <a href="https://web3stick.near.social/" target="_blank" rel="noopener noreferrer">
-          WEB3STICK.NEAR
-        </a>
-      </div>
+      
 
       <div className={styles.navLinks}>
         <Link to="/">HOME</Link>
         <Link to="/playground">PLAYGROUND</Link>
         <Link to="/store">STORE</Link>
-        <Link to="/explore">EXPLORE</Link>
+        {/* <Link to="/explore">EXPLORE</Link> */}
+      </div>
+
+      <div className={styles.socialLink}>
+      WEB3STICK.NEAR<br/>
+      〇 <a href="https://web3stick.near.social/" target="_blank" rel="noopener noreferrer">ONSOCIAL</a>
+
       </div>
 
       <div className="text-uppercase">
-        COPYRIGHT 2025 BY<br/>
-        <a href="https://sleet.near.social/" target="_blank" rel="noopener noreferrer">
-          SLEET.NEAR
-        </a>
+        COPYRIGHT 2025 BY <a href="https://sleet.near.social/" target="_blank" rel="noopener noreferrer">SLEET.NEAR</a>
+        <br/>
+        BUILD A COMMUNITY AROUND ANY NEAR ACCOUNT<br/>
+        CONTACT ME TODAY FOR CUSTOM SOLUTIONS
       </div>
     </footer>
   );
