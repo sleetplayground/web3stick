e6e00cde2710db5fabde32deb69d9783e44bf7e4
The SunShining <thesunshiningmin@gmail.com>
2025-04-22 08:41:05 -0500

    css js

---
+++ src/js/hello.js
@@ -0,0 +1,7 @@
+// Function to log "Hello" to the console
+function logHello() {
+    console.log("👋 Hello, Welcome to Web3Stick");
+}
+
+// Call the function when DOM is ready
+document.addEventListener('DOMContentLoaded', logHello);
