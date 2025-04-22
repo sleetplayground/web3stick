bb8948dffd679ba699acdfd6aa30bd030b88d9a5
The SunShining <thesunshiningmin@gmail.com>
2025-04-22 13:08:09 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import path from 'path';
 
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
+    rollupOptions: {
+      input: Object.fromEntries(
+        glob.sync('src/**/*.html').map(file => [
+          // This removes the `src/` directory prefix and `.html` suffix
+          path.relative('src', file.slice(0, file.length - path.extname(file).length)),
+          // This provides the absolute path to the file
+          fileURLToPath(new URL(file, import.meta.url))
+        ])
+      )
+    }
   },
   server: {
     open: true,
